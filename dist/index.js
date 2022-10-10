7a830dc718480fce7a15cc830e326beed329335f
Tomohiko Ozawa <kota65535@gmail.com>
2022-10-10 13:34:01 +0900

    fix

--- dist/index.js
+++ dist/index.js
@@ -15095,6 +15095,7 @@ const main = async () => {
   core.setOutput('output', jsonString(result.output));
   core.setOutput('warning', jsonString(result.warning));
   core.setOutput('summary', jsonString(result.summary));
+  core.setOutput('should-apply', result.shouldApply);
 };
 
 module.exports = main;

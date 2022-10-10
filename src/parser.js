5ee2e99831c0a43818d5fe4278988a6980dd81d0
Tomohiko Ozawa <kota65535@gmail.com>
2022-10-10 14:33:12 +0900

    fix should-apply

--- src/parser.js
+++ src/parser.js
@@ -120,9 +120,9 @@ const getSummarySection = (inputLines) => {
     let { offset, match } = findLine(inputLines, /^No changes. Your infrastructure matches the configuration.$/);
     return {
       offset,
-      add: [],
-      change: [],
-      destroy: [],
+      add: 0,
+      change: 0,
+      destroy: 0,
       str: match ? match[0] : ''
     };
   }
@@ -141,7 +141,7 @@ const parse = (rawLines) => {
   const warning = getWarningSection(lines);
   const summary = getSummarySection(lines);
 
-  const shouldApply = summary.add.length > 0 || summary.change.length > 0 || summary.destroy.length > 0 || output.sections.length > 0;
+  const shouldApply = summary.add > 0 || summary.change > 0 || summary.destroy > 0 || output.sections.length > 0;
 
   // Handle empty summary string when we have output changes but no resource changes
   if (summary.str === '' && output.sections.length > 0) {

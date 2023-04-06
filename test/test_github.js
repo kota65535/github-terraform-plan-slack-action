2d10ec3014821f76768ea822ab68e443561863c5
Tomohiko Ozawa <kota65535@gmail.com>
2023-04-06 11:01:30 +0900

    No need to checkout (#17)

---
+++ test/test_github.js
@@ -0,0 +1,69 @@
+const { getNumActionsOfSteps, initOctokit, getWorkflow, getJob, getContent } = require("../src/github");
+const assert = require("chai").assert;
+require("dotenv").config();
+
+describe("github", () => {
+  before(async () => {
+    const token = process.env.GITHUB_TOKEN;
+    initOctokit(token);
+  });
+
+  it("gets a workflow", async () => {
+    const workflow = await getWorkflow({
+      repo: {
+        owner: "kota65535",
+        repo: "github-terraform-plan-slack-action",
+      },
+      workflow: "Test",
+    });
+    assert.isNotNull(workflow);
+    assert.isNotNull(workflow.name === "Test");
+  });
+
+  it("gets a job", async () => {
+    const job = await getJob("plan", {
+      repo: {
+        owner: "kota65535",
+        repo: "github-terraform-plan-slack-action",
+      },
+      runId: "4570557599",
+    });
+    assert.isNotNull(job);
+    assert.isNotNull(job.name === "plan");
+  });
+
+  it("gets a repository file", async () => {
+    const file = await getContent(".github/actions/setup-tools/action.yml", {
+      repo: {
+        owner: "kota65535",
+        repo: "github-terraform-plan-slack-action",
+      },
+    });
+    assert.isNotNull(file);
+    assert.isObject(file);
+    assert.isString(file.content);
+  });
+
+  it("gets repository files", async () => {
+    const files = await getContent(".github/workflows", {
+      repo: {
+        owner: "kota65535",
+        repo: "github-terraform-plan-slack-action",
+      },
+    });
+    assert.isNotNull(files);
+    assert.isArray(files);
+    files.forEach((f) => assert.isString(f.content));
+  });
+
+  it("get numbers of each steps", async () => {
+    const numActions = await getNumActionsOfSteps("plan", {
+      repo: {
+        owner: "kota65535",
+        repo: "github-terraform-plan-slack-action",
+      },
+      workflow: "Test",
+    });
+    assert.deepEqual(numActions, [1, 1, 5, 1, 1, 1, 1, 1]);
+  });
+});
